2de18b2a8ce20e3458099d02d932f08b644800b7
Kenyo-cell <kenyo@DESKTOP-6BA945O.localdomain>
2022-11-24 19:49:30 +0300

    feat: ifft implemented

--- main.c
+++ main.c
@@ -6,11 +6,10 @@
 
 typedef double complex cplx;
 #define cx_size sizeof(cplx)
+#define true 1
+#define false 0
 
-cplx *roots;
-int N = 0;
-
-void show(const char * s, cplx buf[], unsigned n) {
+void show(const char *s, cplx buf[], unsigned n) {
     printf("%s", s);
     for (int i = 0; i < n; i++)
         if (!cimag(buf[i]))
@@ -21,12 +20,17 @@ void show(const char * s, cplx buf[], unsigned n) {
     printf("\n");
 }
 
-unsigned rev(unsigned n) {
+unsigned _rev(unsigned n, unsigned N) {
     unsigned reversed = 0;
     for (int i = 0; i < N; i++) {
         reversed |= ((n >> i) & 1) << (N - i - 1);
     }
     return reversed;
+}
+
+unsigned rev(unsigned n) {
+    unsigned N = (int) ceil(log2(n));
+    return _rev(n, N);
 }
 
 cplx* bit_reverse_copy(cplx a[], unsigned n) {
@@ -39,30 +43,31 @@ cplx* bit_reverse_copy(cplx a[], unsigned n) {
     return A;
 }
 
-cplx* fft(cplx* array, unsigned n) {
+int _power(unsigned n) {
     double power = log2(n);
+    
     if (power != trunc(power)) {
         printf("array size is not power of 2\n");
-        return NULL;
+        exit(1);
     }
 
+    return (int)power;
+}
+
+cplx* _fft(cplx* array, unsigned n, short invert) {
+    int power = _power(n);
+    double inverted = invert == true ? -1 : 1;
     cplx *dft = bit_reverse_copy(array, n);
-	cplx t;
-	cplx w;
-	int k;
-	int j;
-	int s;
-	int m;
-	cplx w_m;
-    for (s = 1; s <= (int)power; s++) {
-        m = pow(2, s);
-        w_m = cexp(2 * M_PI * -I / m);
+
+    for (int s = 1; s <= (int)power; s++) {
+        int m = pow(2, s);
+        cplx w_m = cexp(2 * M_PI * -I * inverted / m);
 		
-		#pragma omp parallel for shared(dft, s) private(j, k, t, w)
-        for (k = 0; k < n; k += m) {
-            w = 1;
-            for (j = 0; j < m / 2; j++) {
-                t = w * dft[k + j + m / 2];
+		#pragma omp parallel for shared(dft, s)
+        for (int k = 0; k < n; k += m) {
+            cplx w = 1;
+            for (int j = 0; j < m / 2; j++) {
+                cplx t = w * dft[k + j + m / 2];
                 cplx u = dft[k + j];
                 dft[k + j] = u + t;
                 dft[k + j + m / 2] = u - t;
@@ -70,7 +75,21 @@ cplx* fft(cplx* array, unsigned n) {
             }
         }
     }
+
+    if (invert) {
+        #pragma omp parallel for shared(dft)
+        for (int i = 0; i < n; i++) dft[i] /= n;
+    }
+
     return dft;
+}
+
+cplx* fft(cplx* array, unsigned n) {
+    return _fft(array, n, false);
+}
+
+cplx* ifft(cplx* array, unsigned n) {
+    return _fft(array, n, true);
 }
 
 int main() {
@@ -82,10 +101,14 @@ int main() {
 		buf[i + 2] = 4;
 		buf[i + 3] = -1;
 	}
-    N = (int) ceil(log2(n));
+    
     // show("Data: ", buf, n);
     cplx* res = fft(buf, n);
     // show("\nFFT : ", res, n);
+
+    // show("\nIFFT : ", ifft(res, n), n);
+
+    ifft(res, n);
 
     return 0;
 }
